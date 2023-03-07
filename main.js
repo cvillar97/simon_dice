8a4db873e0d2bd971bfcd58f430e8ed365086509
Cristian <cristianvillar252@gmail.com>
2023-03-07 16:34:17 -0300

    Agrega numeral faltante
    Corrige error.

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let secuenciaMaquina = []
 let secuenciaUsuario = []
 let ronda = 0
 
-const botonJugar = document.querySelector("jugar")
+const botonJugar = document.querySelector("#jugar")
 
 botonJugar.onclick = function () {
     reiniciarJuego()
